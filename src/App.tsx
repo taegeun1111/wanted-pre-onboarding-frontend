17e8d4de5b60d4e534f9bdcf6fb957e8a93260ec
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-11 16:31:41 +0900

    feat : 폴더 구조 설계

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Todo from "./pages/Todo";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn/>}/>
+        <Route path="/signUp" element={<SignUp/>}/>
+        <Route path="/todo" element={<Todo/>}/>
+
+        <Route path="/*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
